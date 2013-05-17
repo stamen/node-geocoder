c0adf88336a78c52e445bf81b33abd64d155d1d9
Sean Connelley <connelley@stamen.comslconnelley@aim.com>
2013-05-17 14:00:11 -0700

    added crossdomain xml path

--- index.js
+++ index.js
@@ -54,6 +54,17 @@ app.get("/", function(req, res) {
     res.jsonp(results);
   });
 });
+app.get( "/crossdomain.xml", onCrossDomainHandler )
+function onCrossDomainHandler( req, res ) {
+  var xml = '<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM' +
+            ' "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n';
+      xml += '<allow-access-from domain="*" to-ports="*"/>\n';
+      xml += '</cross-domain-policy>\n';
+
+  req.setEncoding('utf8');
+  res.writeHead( 200, {'Content-Type': 'text/xml'} );
+  res.end( xml );
+}
 
 app.listen(process.env.PORT || 8080, function() {
   console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
